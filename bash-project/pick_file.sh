62fd413bbcebb84cc8343f70197b859ceb73653a
Olumide Ogundele <olumideralph@gmail.com>
2020-11-10 07:51:50 +0100

    add gnu parallel

--- bash-project/pick_file.sh
+++ bash-project/pick_file.sh
@@ -6,3 +6,10 @@ do
 echo you picked $fname \($REPLY\)
 break;
 done
+
+# using GNU parallel to run commands in parallel, might have to install it.
+{
+echo foss.org.my ;
+echo debian.org ;
+echo freenetproject.org ;
+} | parallel -k traceroute
